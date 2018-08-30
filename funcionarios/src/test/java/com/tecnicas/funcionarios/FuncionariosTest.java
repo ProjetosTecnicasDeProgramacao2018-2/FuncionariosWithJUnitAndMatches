6a3386d71d8a25b513ef395c05b81bf5ea4af4c9
17204196 <17204196@w3873825.portoalegre.pucrsnet.br>
2018-08-30 11:19:40 -0300

    FAZER OS TESTS

--- funcionarios/src/test/java/com/tecnicas/funcionarios/FuncionariosTest.java
+++ funcionarios/src/test/java/com/tecnicas/funcionarios/FuncionariosTest.java
@@ -1,18 +1,16 @@
 package com.tecnicas.funcionarios;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class FuncionariosTest {
-	private Funcionario funcionario;
-
-	public FuncionariosTest(Funcionario funcionario) {
-		super();
-		this.funcionario = funcionario;
-	}
+	private Funcionario funcionario = new Funcionario(12345,"Douglas", 1000, 10, true);
 	
 	@Test
 	public void testAssetSalarioBruto() {
-		this.funcionario.getMatricula();
+		assertEquals(1100, this.funcionario.getSalarioBase());
 	}
 	
 }
